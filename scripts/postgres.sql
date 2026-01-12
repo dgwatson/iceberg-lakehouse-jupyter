8c5162fdea98668bf2d56677d0f7add89de0ad90
David Watson <dave.watson@mac.com>
2026-01-11 23:18:54 -0500

    Added poc schema

--- scripts/postgres.sql
+++ scripts/postgres.sql
@@ -1,6 +1,9 @@
 
+CREATE SCHEMA poc;
 
-CREATE TABLE transactions (
+DROP TABLE poc.transactions;
+
+CREATE TABLE poc.transactions (
     id VARCHAR PRIMARY KEY,
     transaction_date DATE,
     transaction_type VARCHAR,
@@ -9,9 +12,9 @@ CREATE TABLE transactions (
     amount DOUBLE PRECISION
 );
 
-SELECT * FROM transactions;
+SELECT * FROM poc.transactions;
 
-INSERT INTO transactions (id, transaction_date, transaction_type, posted_date, description, amount) VALUES
+INSERT INTO poc.transactions (id, transaction_date, transaction_type, posted_date, description, amount) VALUES
 ('txn_001', '2024-01-01', 'credit', '2024-01-01', 'Initial Deposit', 1000.00),
 ('txn_002', '2024-01-02', 'debit', '2024-01-03', 'Coffee Shop', 4.50),
 ('txn_003', '2024-01-05', 'debit', '2024-01-06', 'Online Bookstore', 25.99);
